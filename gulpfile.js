5e036ad09ccb54bd9c80790643bded7b6285dd15
t32k <ijok.ijok@gmail.com>
2015-01-27 11:00:14 -0800

    Add CSS min file

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,28 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var csscomb = require('gulp-csscomb');
-
-gulp.task('default', function() {
-  // place code for your default task here
-});
-
-gulp.task('default', function () {
-  return gulp.src('src/app.css')
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(gulp.dest('dist'));
-});
+var csslint = require('gulp-csslint');
+var stylestats = require('gulp-stylestats');
+var csso = require('gulp-csso');
 
 
-gulp.task('styles', function() {
+gulp.task('csscomb', function() {
   return gulp.src('css/wisteria.css')
     .pipe(csscomb())
     .pipe(gulp.dest('./build/css'));
 });
-
-var csslint = require('gulp-csslint');
-
 gulp.task('test', function() {
   gulp.src('css/wisteria.css')
     .pipe(csslint('.csslintrc'))
     .pipe(csslint.reporter())
     .pipe(csslint.failReporter());
+});
+gulp.task('stylestats', function () {
+  gulp.src('css/wisteria.css')
+    .pipe(stylestats());
+});
+gulp.task('csso', function() {
+  return gulp.src('css/wisteria.css')
+    .pipe(csso())
+    .pipe(gulp.dest('css/wisteria.min.css'));
 });

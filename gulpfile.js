9d8f6ceff423e9e31351880b0fd234f1a1d1293a
t32k <ijok.ijok@gmail.com>
2015-01-27 13:45:42 -0800

    Add min file

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,22 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
-gulp.task('test', function() {
+gulp.task('default', function() {
   gulp.src('css/wisteria.css')
+    .pipe($.csscomb())
+    .pipe(gulp.dest('css/'))
+    .pipe($.rename({suffix: '.min'}))
+    .pipe($.csso())
+    .pipe(gulp.dest('css/'));
+});
+
+gulp.task('test', function() {
+  gulp.src('./css/wisteria.css')
     .pipe($.csslint('.csslintrc'))
     .pipe($.csslint.reporter())
     .pipe($.csslint.failReporter());
 });
-gulp.task('csso', function() {
-  return gulp.src('css/wisteria.css')
-    .pipe($.csso())
-    .pipe(gulp.dest('css/dist/'));
-});
-gulp.task('csscomb', function() {
-  return gulp.src('./css/wisteria.css')
-    .pipe($.csscomb())
-    .pipe(gulp.dest('./css/'));
-});
+
 gulp.task('report', function () {
   gulp.src('./css/wisteria.css')
     .pipe($.stylestats());

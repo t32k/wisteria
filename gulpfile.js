ff3bba510a35e7b1d8e16a6f04cba4d083b6c97f
t32k <ijok.ijok@gmail.com>
2015-01-27 20:32:05 -0800

    Change dir

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,25 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
 gulp.task('default', function() {
-  gulp.src('css/wisteria.css')
+  gulp.src('./wisteria.css')
+    .pipe($.csslint('.csslintrc'))
+    .pipe($.csslint.reporter())
+    .pipe($.csslint.failReporter())
     .pipe($.csscomb())
-    .pipe(gulp.dest('css/'))
+    .pipe(gulp.dest('./'))
     .pipe($.rename({suffix: '.min'}))
     .pipe($.csso())
-    .pipe(gulp.dest('css/'));
+    .pipe(gulp.dest('./public/css/'));
 });
 
 gulp.task('test', function() {
-  gulp.src('./css/wisteria.css')
+  gulp.src('./wisteria.css')
     .pipe($.csslint('.csslintrc'))
     .pipe($.csslint.reporter())
     .pipe($.csslint.failReporter());
 });
 
 gulp.task('report', function () {
-  gulp.src('./css/wisteria.css')
+  gulp.src('./wisteria.css')
     .pipe($.stylestats());
 });
